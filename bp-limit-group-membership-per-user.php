07f0af6507c8720b4563b702dbe6aa513030da06
Brajesh Singh <brajesh@cosmiccoders.com>
2012-06-05 07:08:47 +0530

    just some indentation

--- bp-limit-group-membership-per-user.php
+++ bp-limit-group-membership-per-user.php
@@ -17,12 +17,12 @@ class BPLimitGroupMembership{
     
     private function __construct() {
         
-        add_action('wp_footer',array($this,'ouput_js'),200);
-        add_action('bp_get_group_join_button',array($this,'fix_join_button'),100);
-        add_filter('bp_groups_auto_join',array($this,'can_join'));
-        add_filter('bp_core_admin_screen',array($this,'limit_group_join_admin_screen'));
-        add_action('wp',array($this,'check_group_create'),2);
-        add_action('init',array($this,'remove_hook'),2);
+        add_action('wp_footer',array($this,'ouput_js'),200);//we filter the invite list using javascript, in 1.6, we won't need it
+        add_action('bp_get_group_join_button',array($this,'fix_join_button'),100);//remove the group join button
+        add_filter('bp_groups_auto_join',array($this,'can_join'));///check if we can allow autojoin
+        add_filter('bp_core_admin_screen',array($this,'limit_group_join_admin_screen'));//show super admin option to set the maximum no.
+        add_action('wp',array($this,'check_group_create'),2);//check if group can be created
+        add_action('init',array($this,'remove_hook'),2);//remove the bp hooks
        //ajaxed join/leave
         add_action( 'wp_ajax_joinleave_group', array($this,'ajax_joinleave_group') );
         //for normal bp action(when a user opens the join link), thanks to Matteo
@@ -206,89 +206,93 @@ class BPLimitGroupMembership{
 
 	bp_core_load_template( apply_filters( 'groups_template_group_home', 'groups/single/home' ) );
    } 
+   
+   
     //remove ajax handler
     //currently works for bp 1.5 bp-default theme
     function remove_hook(){
         remove_action( 'wp_ajax_joinleave_group', 'bp_dtheme_ajax_joinleave_group' );
         remove_action( 'bp_actions', 'groups_action_join_group' );
     }
-    //do not allow inviting the members who have exhusted their limit
-    function ouput_js(){
-        //load only on group create and group invite pages
-     
-     if(!(bp_is_group_creation_step( 'group-invites' )||bp_is_group_invites()) )
-         return;
-     ///fields to restrict
-     $users=self::get_friends_not_to_invite();
-?>
-
-<script type='text/javascript'>
-    var group_member_restriction_list=<?php echo json_encode($users).";";?>
-    var count=group_member_restriction_list.length;
-    for(var i=0;i<count;i++){
-        jQuery("input#f-"+group_member_restriction_list[i]).prop('disabled',true);
-    }
     
-</script>
-     
-<?php
+    
+    //do not allow inviting the members who have exhusted their limit
+        function ouput_js(){
+            //load only on group create and group invite pages
+
+         if(!(bp_is_group_creation_step( 'group-invites' )||bp_is_group_invites()) )
+             return;
+         ///fields to restrict
+         $users=self::get_friends_not_to_invite();
+    ?>
+
+    <script type='text/javascript'>
+        var group_member_restriction_list=<?php echo json_encode($users).";";?>
+        var count=group_member_restriction_list.length;
+        for(var i=0;i<count;i++){
+            jQuery("input#f-"+group_member_restriction_list[i]).prop('disabled',true);
+        }
+
+    </script>
+
+    <?php
     } 
     
-  /**
+/**
  * Show the option on BuddyPress settings page to Limit the group
  */
-function limit_group_join_admin_screen(){
-?>
-<table class="form-table">
-<tbody>
-<tr>
-	<th scope="row"><?php _e( 'Limit Groups Membership Per User' ) ?></th>
-		<td>
-                    <p><?php _e( 'How many Groups a user can join?') ?></p>
-                    <label><input type="text" name="bp-admin[group_membership_limit]" id="group_membership_limit" value="<?php echo bp_get_option( 'group_membership_limit',0 );?>" /></label><br>
-                </td>
-	</tr>
-</tbody>
-</table>				
-<?php
-}  
+    function limit_group_join_admin_screen(){
+    ?>
+    <table class="form-table">
+    <tbody>
+    <tr>
+            <th scope="row"><?php _e( 'Limit Groups Membership Per User' ) ?></th>
+                    <td>
+                        <p><?php _e( 'How many Groups a user can join?') ?></p>
+                        <label><input type="text" name="bp-admin[group_membership_limit]" id="group_membership_limit" value="<?php echo bp_get_option( 'group_membership_limit',0 );?>" /></label><br>
+                    </td>
+            </tr>
+    </tbody>
+    </table>				
+    <?php
+    }  
 
 
-function restrict_group_create($user_id=null){
-	global $bp;
+    function restrict_group_create($user_id=null){
+            global $bp;
 
-    //no restriction to site admin
-    if (!bp_is_group_create() ||is_super_admin())
-		return false;
-    //if we are here,It is group creation step
+        //no restriction to site admin
+        if (!bp_is_group_create() ||is_super_admin())
+                    return false;
+        //if we are here,It is group creation step
 
-    if(!$user_id)
-	$user_id=$bp->loggedin_user->id;
-    //even in cae of zero, it will return true
-    if(!empty($_COOKIE['bp_new_group_id']))
-        return;//this is intermediate step of group creation
-    
-    if(!self::can_join()){
+        if(!$user_id)
+            $user_id=$bp->loggedin_user->id;
+        //even in cae of zero, it will return true
+        if(!empty($_COOKIE['bp_new_group_id']))
+            return;//this is intermediate step of group creation
 
-		bp_core_add_message(apply_filters('restrict_group_membership_message',__("You already have the maximum no. of groups allowed. You can not create or join new groups!")),'error');
-		remove_action( 'wp', 'groups_action_create_group', 3 );
-		bp_core_redirect(bp_get_root_domain().'/'.  bp_get_groups_slug());
+        if(!self::can_join()){
+
+                    bp_core_add_message(apply_filters('restrict_group_membership_message',__("You already have the maximum no. of groups allowed. You can not create or join new groups!")),'error');
+                    remove_action( 'wp', 'groups_action_create_group', 3 );
+                    bp_core_redirect(bp_get_root_domain().'/'.  bp_get_groups_slug());
+        }
+
+
     }
+    /**
+     * Check if we should allow creating group or not
+     * @global type $bp
+     * @return type 
+     */
+    function check_group_create(){
+            global $bp;
+            if(!function_exists('bp_is_active')||!bp_is_active('groups'))
+                    return; //do not cause headache
 
-
-}
-/**
- * Check if we should allow creating group or not
- * @global type $bp
- * @return type 
- */
-function check_group_create(){
-	global $bp;
-	if(!function_exists('bp_is_active')||!bp_is_active('groups'))
-		return; //do not cause headache
-	
-	self::restrict_group_create();
-}
+            self::restrict_group_create();
+    }
 
 
 }
