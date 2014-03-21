7040eb1bdb99a4b3be75a8a38d1832fd806eae11
Brajesh Singh <brajesh@cosmiccoders.com>
2014-03-21 20:26:44 +0530

    Update and add the filter bp_limit_group_membership_limit to allow flexibility in allowed count

--- bp-limit-group-membership-per-user.php
+++ bp-limit-group-membership-per-user.php
@@ -4,7 +4,7 @@
  * Plugin URI:http://buddydev.com/plugins/bp-limit-group-membership/
  * Author: Brajesh Singh
  * Author URI: http://buddydev.com/members/sbrajesh
- * Version : 1.0.3
+ * Version : 1.0.4
  * License: GPL
  * Description: Restricts the no. of Groups a user can join
  */
@@ -17,31 +17,31 @@ class BPLimitGroupMembership{
     
     private function __construct() {
         
-        add_action('wp_footer',array($this,'ouput_js'),200);//we filter the invite list using javascript, in 1.6, we won't need it
-        add_action('bp_get_group_join_button',array($this,'fix_join_button'),100);//remove the group join button
-        add_filter('bp_groups_auto_join',array($this,'can_join'));///check if we can allow autojoin
+        add_action( 'wp_footer', array( $this, 'ouput_js' ), 200 );//we filter the invite list using javascript, in 1.6, we won't need it
+        add_action( 'bp_get_group_join_button', array( $this, 'fix_join_button' ), 100 );//remove the group join button
+        add_filter( 'bp_groups_auto_join', array( $this, 'can_join' ) );///check if we can allow autojoin
        
-        add_action('wp',array($this,'check_group_create'),2);//check if group can be created
-        add_action('init',array($this,'remove_hook'),2);//remove the bp hooks
+        add_action( 'wp', array( $this, 'check_group_create' ), 2 );//check if group can be created
+        add_action( 'init', array( $this, 'remove_hook' ), 2 );//remove the bp hooks
        //ajaxed join/leave
-        add_action( 'wp_ajax_joinleave_group', array($this,'ajax_joinleave_group') );
+        add_action( 'wp_ajax_joinleave_group', array( $this, 'ajax_joinleave_group' ) );
         //for normal bp action(when a user opens the join link), thanks to Matteo
-        add_action( 'bp_actions', array($this,'action_join_group') );
+        add_action( 'bp_actions', array( $this, 'action_join_group' ) );
         
         //add_filter('bp_core_admin_screen',array($this,'limit_group_join_admin_screen'));//show super admin option to set the maximum no.
     }
     
-    function get_instance(){
-        if(!isset (self::$instance))
-            self::$instance=new self();
+    public static function get_instance(){
+        if( !isset ( self::$instance ) )
+            self::$instance = new self();
         
         return self::$instance;
     }
     
-    function get_limit(){
-        return bp_get_option('group_membership_limit',0);
+    public function get_limit(){
+        return apply_filters( 'bp_limit_group_membership_limit', bp_get_option( 'group_membership_limit', 0 ) );
     }
-    function get_group_count($user_id){
+    public function get_group_count( $user_id ){
         global $bp, $wpdb;
 		
 	return $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(DISTINCT m.group_id) FROM {$bp->groups->table_name_members} m WHERE m.user_id = %d AND m.is_confirmed = 1 AND m.is_banned = 0", $user_id ) );
@@ -49,18 +49,18 @@ class BPLimitGroupMembership{
     }
     public static function can_join(){
        
-        $limit=self::get_limit();
-        if(is_super_admin())
+        $limit = self::get_limit();
+        if( is_super_admin() )
             return true;
         //if user is not logged in or the limit is set to zero, return false
-        if(!(is_user_logged_in()&&$limit))
+        if( !( is_user_logged_in() && $limit ) )
             return false;
         
-        $user_id=bp_loggedin_user_id();
+        $user_id = bp_loggedin_user_id();
         //check how many groups the user has already joined
-        $group_count=self::get_group_count($user_id);//get_user_meta( $user_id, 'total_group_count', true );
+        $group_count = self::get_group_count( $user_id );//get_user_meta( $user_id, 'total_group_count', true );
       
-        if($group_count<$limit)
+        if( $group_count < $limit )
             return true;
         
         return false;
@@ -68,33 +68,34 @@ class BPLimitGroupMembership{
     
    //prevent listing of members for invitation
     
-    function filter_invite_list(){
+    public function filter_invite_list(){
         
     }
     
-    function  get_friends_not_to_invite(){
-        global $wpdb,$bp;
-        $user_id=bp_loggedin_user_id();
-        $limit=self::get_limit();
+    public function  get_friends_not_to_invite(){
+        global $wpdb, $bp;
+        $user_id = get_current_user_id();
+        $limit = self::get_limit();
         //get all friends who can not be invited
-        $user_ids=friends_get_friend_user_ids($user_id);
+        $user_ids = friends_get_friend_user_ids( $user_id );
         
-        if(empty($user_ids))
+        if( empty( $user_ids ) )
             return array();
         
-        $user_list='('.join(',',$user_ids).')';
+        $user_list = '(' . join( ',', $user_ids ) . ')';
         ///find all the users who have not exhusted the mebership count
         
         
         
-       $query="SELECT user_id, count(group_id) as gcount FROM {$bp->groups->table_name_members} WHERE user_id IN {$user_list} order by user_id";
+       $query = "SELECT user_id, COUNT(group_id) AS gcount FROM {$bp->groups->table_name_members} WHERE user_id IN {$user_list} ORDER BY user_id";
       
-       $query=$wpdb->prepare($query,$limit);
-       $selected=array();
-       $results=$wpdb->get_results($query);
-       foreach((array)$results as $row){
-           if($row->gcount>$limit)
-               $selected[]=$row->user_id;
+       $query = $wpdb->prepare( $query, $limit );
+       $selected = array();
+       $results = $wpdb->get_results( $query );
+       foreach( (array) $results as $row ){
+           
+           if( $row->gcount > $limit )
+               $selected[] = $row->user_id;
        }
        return $selected;
        
@@ -105,13 +106,15 @@ class BPLimitGroupMembership{
     
     
     //hide the join button
-    function  fix_join_button($btn){
+    public function  fix_join_button( $btn ){
         
-        if(self::can_join())
+        if( self::can_join() )
             return $btn;
+        
         //otherwise check if the button is for requesting membership
-        if($btn['id']=='request_membership'||$btn['id']=='join_group')
-        $btn='';
+        if( $btn['id'] == 'request_membership' || $btn['id'] == 'join_group' )
+            $btn = '';
+        
         return $btn;
     }
     
@@ -119,7 +122,7 @@ class BPLimitGroupMembership{
     /* AJAX join or leave a group when clicking the "join/leave" button
      * A copy of bp_dtheme_ajax_joinleave_group function modified for our purpose
      *  */
-    function ajax_joinleave_group() {
+    public function ajax_joinleave_group() {
 	global $bp;
 
 	if ( groups_is_user_banned( $bp->loggedin_user->id, $_POST['gid'] ) )
@@ -129,7 +132,7 @@ class BPLimitGroupMembership{
 		return false;
 
 	if ( !groups_is_user_member( $bp->loggedin_user->id, $group->id ) ) {
-            if(!self::can_join()){
+            if( !self::can_join() ){
                 echo apply_filters('restrict_group_membership_message',__("You already have the maximum no. of groups allowed. You can not create or join new groups!"));
 		return;
             }   
@@ -170,7 +173,7 @@ class BPLimitGroupMembership{
 	}
     }
     
-   function action_join_group(){
+   public function action_join_group(){
        global $bp;
 
 	if ( !bp_is_single_item() || !bp_is_groups_component() || !bp_is_current_action( 'join' ) )
@@ -211,9 +214,13 @@ class BPLimitGroupMembership{
    
     //remove ajax handler
     //currently works for bp 1.5 bp-default theme
-    function remove_hook(){
-        remove_action( 'wp_ajax_joinleave_group', 'bp_dtheme_ajax_joinleave_group' );
-        remove_action( 'bp_actions', 'groups_action_join_group' );
+    public function remove_hook(){
+        if( has_action( 'wp_ajax_joinleave_group', 'bp_dtheme_ajax_joinleave_group' ) )
+            remove_action( 'wp_ajax_joinleave_group', 'bp_dtheme_ajax_joinleave_group' );
+        
+        if( has_action( 'bp_actions', 'groups_action_join_group' ) )
+            remove_action( 'bp_actions', 'groups_action_join_group' );
+        
     }
     
     
@@ -221,17 +228,17 @@ class BPLimitGroupMembership{
         function ouput_js(){
             //load only on group create and group invite pages
 
-         if(!(bp_is_group_creation_step( 'group-invites' )||bp_is_group_invites()) )
+         if( !( bp_is_group_creation_step( 'group-invites' ) || bp_is_group_invites() ) )
              return;
          ///fields to restrict
-         $users=self::get_friends_not_to_invite();
+         $users = self::get_friends_not_to_invite();
     ?>
 
     <script type='text/javascript'>
-        var group_member_restriction_list=<?php echo json_encode($users).";";?>
-        var count=group_member_restriction_list.length;
-        for(var i=0;i<count;i++){
-            jQuery("input#f-"+group_member_restriction_list[i]).prop('disabled',true);
+        var group_member_restriction_list = <?php echo json_encode($users).";";?>
+        var count = group_member_restriction_list.length;
+        for( var i=0; i<count; i++ ){
+            jQuery( "input#f-"+ group_member_restriction_list[i] ).prop( 'disabled', true );
         }
 
     </script>
@@ -242,7 +249,7 @@ class BPLimitGroupMembership{
  
 
 
-    function restrict_group_create($user_id=null){
+    public function restrict_group_create( $user_id=null ){
             global $bp;
 
         //no restriction to site admin
@@ -250,13 +257,13 @@ class BPLimitGroupMembership{
                     return false;
         //if we are here,It is group creation step
 
-        if(!$user_id)
-            $user_id=$bp->loggedin_user->id;
+        if( !$user_id )
+            $user_id = get_current_user_id();
         //even in cae of zero, it will return true
-        if(!empty($_COOKIE['bp_new_group_id']))
+        if( !empty( $_COOKIE['bp_new_group_id'] ) )
             return;//this is intermediate step of group creation
 
-        if(!self::can_join()){
+        if( !self::can_join() ){
 
                     bp_core_add_message(apply_filters('restrict_group_membership_message',__("You already have the maximum no. of groups allowed. You can not create or join new groups!")),'error');
                     remove_action( 'wp', 'groups_action_create_group', 3 );
@@ -270,9 +277,9 @@ class BPLimitGroupMembership{
      * @global type $bp
      * @return type 
      */
-    function check_group_create(){
+    public function check_group_create(){
             global $bp;
-            if(!function_exists('bp_is_active')||!bp_is_active('groups'))
+            if( !function_exists( 'bp_is_active' ) || !bp_is_active( 'groups' ) )
                     return; //do not cause headache
 
             self::restrict_group_create();
@@ -286,15 +293,17 @@ BPLimitGroupMembership::get_instance();
 class BPLimitGroupMembershipAdminHelper{
     private static $instance;
     
-    function __construct() {
-        add_Action('bp_admin_init',array($this,'register_settings'),20);
+    private function __construct() {
+        add_action( 'bp_admin_init', array( $this, 'register_settings' ), 20 );
     }
-     function get_instance(){
-     if(!isset (self::$instance))
-             self::$instance=new self();
-     return self::$instance;
+    
+    public static function get_instance(){
+        if( !isset ( self::$instance ) )
+                self::$instance = new self();
+        return self::$instance;
     }
-    function register_settings(){
+    
+    public function register_settings(){
         // Add the ajax Registration settings section
             add_settings_section( 'bp_limit_group_membership',        __( 'Limit Group membership Settings',  'bp-limit-group-membership' ), array($this,'reg_section'),   'buddypress'              );
             // Allow loading form via jax or nt?
@@ -307,7 +316,7 @@ class BPLimitGroupMembershipAdminHelper{
     }
     
     function settings_field(){
-            $val=bp_get_option('group_membership_limit',0);?>
+            $val = bp_get_option('group_membership_limit',0);?>
 
          
                    
@@ -318,4 +327,3 @@ class BPLimitGroupMembershipAdminHelper{
     
 }
 BPLimitGroupMembershipAdminHelper::get_instance();
-?>
